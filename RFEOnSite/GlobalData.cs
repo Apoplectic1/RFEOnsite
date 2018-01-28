cdbf1805df1de06292e155671faee97cdc3e9390
Dan <djstark@gmail.com>
2018-01-28 00:19:27 -0500

    Fixed Frequency Overlap

--- RFEOnSite/GlobalData.cs
+++ RFEOnSite/GlobalData.cs
@@ -158,22 +158,22 @@ namespace RFEOnSite
             //mPairs.Add(new PresetTableEntry(2149.8, 2161.0, eBand.eAWS));
 
             mPairs.Add(new PresetTableEntry(728.0, 739.1, eBand.e700));
-            mPairs.Add(new PresetTableEntry(739.1, 750.2, eBand.e700));
-            mPairs.Add(new PresetTableEntry(750.2, 761.3, eBand.e700));
+            mPairs.Add(new PresetTableEntry(739.2, 750.3, eBand.e700));
+            mPairs.Add(new PresetTableEntry(750.4, 761.5, eBand.e700));
             mPairs.Add(new PresetTableEntry(865.0, 876.1, eBand.e850));
-            mPairs.Add(new PresetTableEntry(876.1, 887.2, eBand.e850));
-            mPairs.Add(new PresetTableEntry(887.2, 898.3, eBand.e850));
+            mPairs.Add(new PresetTableEntry(876.2, 887.3, eBand.e850));
+            mPairs.Add(new PresetTableEntry(887.4, 898.5, eBand.e850));
             mPairs.Add(new PresetTableEntry(1929.0, 1940.1, eBand.ePCS));
-            mPairs.Add(new PresetTableEntry(1940.1, 1951.2, eBand.ePCS));
-            mPairs.Add(new PresetTableEntry(1951.2, 1962.3, eBand.ePCS));
-            mPairs.Add(new PresetTableEntry(1962.3, 1973.4, eBand.ePCS));
-            mPairs.Add(new PresetTableEntry(1973.4, 1984.5, eBand.ePCS));
-            mPairs.Add(new PresetTableEntry(1984.5, 1995.6, eBand.ePCS));
+            mPairs.Add(new PresetTableEntry(1940.2, 1951.3, eBand.ePCS));
+            mPairs.Add(new PresetTableEntry(1951.4, 1962.5, eBand.ePCS));
+            mPairs.Add(new PresetTableEntry(1962.6, 1973.7, eBand.ePCS));
+            mPairs.Add(new PresetTableEntry(1973.8, 1984.9, eBand.ePCS));
+            mPairs.Add(new PresetTableEntry(1985.0, 1996.1, eBand.ePCS));
             mPairs.Add(new PresetTableEntry(2105.0, 2116.1, eBand.eAWS));
-            mPairs.Add(new PresetTableEntry(2116.1, 2127.2, eBand.eAWS));
-            mPairs.Add(new PresetTableEntry(2127.2, 2138.3, eBand.eAWS));
-            mPairs.Add(new PresetTableEntry(2138.3, 2149.4, eBand.eAWS));
-            mPairs.Add(new PresetTableEntry(2149.4, 2160.5, eBand.eAWS));
+            mPairs.Add(new PresetTableEntry(2116.2, 2127.3, eBand.eAWS));
+            mPairs.Add(new PresetTableEntry(2127.4, 2138.5, eBand.eAWS));
+            mPairs.Add(new PresetTableEntry(2138.6, 2149.7, eBand.eAWS));
+            mPairs.Add(new PresetTableEntry(2149.8, 2160.9, eBand.eAWS));
         }
 
         public IEnumerator<PresetTableEntry> GetEnumerator()
